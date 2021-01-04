ffe90ddaa39f38090fbcdad757eeb8d8cb4fde3c
Florian Uhde <floar@mx.de>
2021-01-04 14:36:20 +0100

    rmtemplate module for working with remarkable templates

---
+++ rmtemplates/go.mod
@@ -0,0 +1,5 @@
+module github.com/floar/remarkablego/rmtemplates
+
+go 1.15
+
+require muzzammil.xyz/jsonc v0.0.0-20201229145248-615b0916ca38
---
+++ rmtemplates/rmtemplates.go
@@ -0,0 +1,58 @@
+package rmtemplates
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	"muzzammil.xyz/jsonc"
+)
+
+type Template struct {
+	Name       string   `json:"name"`
+	Filename   string   `json:"filename"`
+	IconCode   string   `json:"iconCode"`
+	Categories []string `json:"categories"`
+	Landscape  bool     `json:"landscape,omitempty"`
+}
+
+type TemplatesMaster struct {
+	Templates []Template `json:"templates"`
+}
+
+func (tm TemplatesMaster) HasTemplateWithName(name string) bool {
+	for i := 0; i < len(tm.Templates); i++ {
+		if tm.Templates[i].Name == name {
+			return true
+		}
+	}
+	return false
+}
+
+func (tm TemplatesMaster) HasTemplateForFile(filename string) bool {
+	for i := 0; i < len(tm.Templates); i++ {
+		if tm.Templates[i].Filename == filename {
+			return true
+		}
+	}
+	return false
+}
+
+func (tm TemplatesMaster) Append(newTemplate Template) TemplatesMaster {
+	tm.Templates = append(tm.Templates, newTemplate)
+	return tm
+}
+
+func LoadTemplateMaster(templatesFile string) TemplatesMaster {
+	data, err := ioutil.ReadFile(templatesFile)
+	if err != nil {
+		fmt.Print(err)
+	}
+	jc := jsonc.ToJSON(data) // Calling jsonc.ToJSON() to convert JSONC to JSON
+	var tMaster TemplatesMaster
+	err = json.Unmarshal([]byte(jc), &tMaster)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	return tMaster
+}
